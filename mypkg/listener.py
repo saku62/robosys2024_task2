0ed1bc4e213286290fdacc7492681179ae31dc80
Yuta Sakusabe <s23c1062mq@s.chibakoudai.jp>
2024-12-20 10:35:53 +0900

    ?

--- mypkg/listener.py
+++ mypkg/listener.py
@@ -2,15 +2,16 @@ import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Int16
 
+
 rclpy.init()
 node = Node("listener")
 
 
 def cb(msg):
     global node
-    node.get_logger().info("Listen: %s" % msg)
+    node.get_logger().info("Listen: %d" % data)
 
 
 def main():
-    sub = node.create_subscription(Person, "person", cb, 10)
+    sub = node.create_subscription(Int16, "countup", cb, 10)
     rclpy.spin(node)
